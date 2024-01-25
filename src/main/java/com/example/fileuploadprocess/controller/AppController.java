2122c8e103f07c93606885fba11e938cddc4a257
trunglq <anzure1234@gmail.com>
2024-01-25 22:14:57 +0700

    123

--- src/main/java/com/example/fileuploadprocess/controller/AppController.java
+++ src/main/java/com/example/fileuploadprocess/controller/AppController.java
@@ -5,13 +5,21 @@ import com.example.fileuploadprocess.dto.ResponseDTO;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/file")
 public class AppController {
 
     @PostMapping
-    public ResponseDTO<FileDTO> uploadFile(@RequestParam("content") MultipartFile fileDTO) {
-        return new ResponseDTO("Received file", fileDTO.getOriginalFilename(), 200);
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseDTO<List<FileDTO>> uploadFiles(@RequestParam("content") List<MultipartFile> files) {
+        files.forEach(file -> System.out.println("run here"));
+        List<FileDTO> fileDTOs = files.stream()
+                .map(file -> new FileDTO(file.getOriginalFilename(), file.getContentType(), file))
+                .collect(Collectors.toList());
+        return new ResponseDTO("Received files", fileDTOs, 200);
     }
 
     @GetMapping
